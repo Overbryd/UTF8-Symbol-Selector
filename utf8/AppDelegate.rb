8908d0472769ccf51acd5b530412eee91b1587a1
Lukas Rieder <l.rieder@gmail.com>
2012-02-02 13:57:59 +0100

    Initial Commit

---
+++ utf8/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  utf8
+#
+#  Created by Lukas Rieder on 2/2/12.
+#  Copyright 2012 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+  attr_accessor :window
+  def applicationDidFinishLaunching(a_notification)
+    # Insert code here to initialize your application
+  end
+end
+
---
+++ utf8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  utf8
+//
+//  Created by Lukas Rieder on 2/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+  return macruby_main("rb_main.rb", argc, argv);
+}
